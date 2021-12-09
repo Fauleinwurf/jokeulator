a354bcd24341f9b8e7207fb4b11be657a451992f
Nino Oirfun <nino.nufrio@gmail.com>
2021-12-09 09:25:58 +0100

    - naming changes
    Signed-off-by: Nino Oirfun <nino.nufrio@gmail.com>

---
+++ app/src/main/java/ch/nn/jokeulator/model/JokeApi.java
@@ -0,0 +1,8 @@
+package ch.nn.jokeulator.model;
+
+public enum JokeApi {
+    ;
+
+    public static final String CHUCK_NORRIS_JOKES = "https://api.chucknorris.io/jokes/random";
+    public static final String DAD_JOKES = "api";
+}

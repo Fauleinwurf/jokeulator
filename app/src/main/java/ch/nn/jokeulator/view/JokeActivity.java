2fc1c0dc69bcefdb4164fc23a5c499c9ecba604c
Nino Oirfun <nino.nufrio@gmail.com>
2021-12-09 10:48:35 +0100

    - retrieve extras in JokeActivity
    Signed-off-by: Nino Oirfun <nino.nufrio@gmail.com>

--- app/src/main/java/ch/nn/jokeulator/view/JokeActivity.java
+++ app/src/main/java/ch/nn/jokeulator/view/JokeActivity.java
@@ -1,18 +1,31 @@
 package ch.nn.jokeulator.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import ch.nn.jokeulator.R;
+import ch.nn.jokeulator.model.JokeApi;
 
 public class JokeActivity extends AppCompatActivity {
+
+    private JokeApi jokeApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+
+        this.initExtras();
+
+        TextView categoryTitle = findViewById(R.id.joke_category);
+        categoryTitle.setText(jokeApi.name);
+    }
+
+    private void initExtras() {
+        this.jokeApi = new JokeApi(getIntent().getStringExtra("jokeApi"), getIntent().getStringExtra("jokeApiName"));
     }
 
     public void skipJoke(View view) {

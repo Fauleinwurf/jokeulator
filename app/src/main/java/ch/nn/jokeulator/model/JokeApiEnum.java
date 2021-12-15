bd9b02d6f5e56bb48af4fe2ec12228d45c958201
Nino Oirfun <nino.nufrio@gmail.com>
2021-12-15 09:20:55 +0100

    - removed unnecessary dadJokeApi class
    Signed-off-by: Nino Oirfun <nino.nufrio@gmail.com>

--- app/src/main/java/ch/nn/jokeulator/model/JokeApiEnum.java
+++ app/src/main/java/ch/nn/jokeulator/model/JokeApiEnum.java
@@ -1,8 +1,37 @@
 package ch.nn.jokeulator.model;
 
+import android.util.JsonReader;
+
+import java.io.IOException;
+
+import ch.nn.jokeulator.service.JokeService;
+
 public enum JokeApiEnum {
     CHUCK_NORRIS(new JokeApi("https://api.chucknorris.io/jokes/random/", "Chuck Norris jokes", "value")),
-    DAD(new DadJokeApi("https://api.dadjokes.io/api/random/joke/", "Dad Jokes", "setup"));
+    DAD(new JokeApi("https://api.dadjokes.io/api/random/joke/", "Dad Jokes", "setup"){
+        @Override
+        public String getJokeJson(JsonReader reader) throws IOException {
+
+            reader.beginObject();
+            reader.nextName();
+            reader.skipValue();
+            reader.nextName();
+            reader.beginArray();
+            reader.beginObject();
+            StringBuilder joke = new StringBuilder(JokeService.DEFAULT_JOKE_TEXT);
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+                if (name.equals(this.jsonLabel)) {
+                    joke.append(reader.nextString()).append("\n\n");
+                    reader.nextName();
+                    joke.append(reader.nextString()).append("\n\n");
+                } else {
+                    reader.skipValue();
+                }
+            }
+            return joke.toString();
+        }
+    });
 
     public final JokeApi api;
 
